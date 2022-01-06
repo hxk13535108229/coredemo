d6b62fe964ddd0ed45e81897443b3e7c04cf932b
3119004966_hxk <1429195907@qq.com>
2022-01-06 20:01:40 +0800

    01

---
+++ framework/core.go
@@ -0,0 +1,16 @@
+package framework
+
+import "net/http"
+
+type Core struct{
+
+}
+
+func NewCore() *Core{
+	return &Core{}
+}
+
+func (c *Core) ServeHTTP(response http.ResponseWriter,request *http.Request){
+	//TODO
+	
+}
